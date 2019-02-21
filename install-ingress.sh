ae4a7062d835d5e5fd70b74b01ec18d8381441ee
Marcello Teodori <marcello.teodori@alfresco.com>
2019-02-21 12:48:43 +0000

    ACTIVITI-2594 added info to pass a custom nginx template

--- install-ingress.sh
+++ install-ingress.sh
@@ -1,46 +1,91 @@
 #!/usr/bin/env bash
 
 # CERT ARN for envalfresco.com
-APS_DOMAIN="${APS_HOST#*.*}"
-APS_INGRESS_AWS_CERT_ARN=${APS_INGRESS_AWS_CERT_ARN:-$(aws acm list-certificates --query "CertificateSummaryList[?contains(DomainName,'${APS_DOMAIN}')].CertificateArn" --output text)}
+APS_DOMAIN="${APS_DOMAIN:-${APS_HOST#*.*}}"
+APS_INGRESS_AWS_CERT_ARN=${APS_INGRESS_AWS_CERT_ARN:-$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='${APS_DOMAIN}'].CertificateArn" --output text)}
 env | grep APS | sort
+
+# custom headers config https://kubernetes.github.io/ingress-nginx/examples/customization/custom-headers/
+# configmap can be edited and hot reload will happen: kubectl edit configmaps nginx-ingress-controller
+# see the last line on how to test: kubectl exec nginx-ingress-controller-<pod_name> cat /etc/nginx/nginx.conf
+
+kubectl create configmap nginx-template --from-file nginx.tmpl
 
 cat << EOF > values.yaml
 controller:
-  publishService:
-    enabled: true
+#  publishService:
+#    enabled: true
+  service:
+    enableHttps: false
   config:
-    ssl-redirect: "false"
-    use-proxy-protocol: "false"
+#    generate-request-id: "true"
+    proxy-read-timeout: "3600"
+    proxy-send-timeout: "3600"
+    proxy-set-headers: "default/nginx-ingress-custom-headers"
+#    ssl-redirect: "false"
+#    use-forwarded-headers: "true"
+    use-proxy-protocol: "true"
+volumeMounts:
+  - mountPath: /etc/nginx/template
+    name: nginx-template-volume
+    readOnly: true
+volumes:
+  - name: nginx-template-volume
+    configMap:
+      name: nginx-template
+      items:
+      - key: nginx.tmpl
+        path: nginx.tmpl
 EOF
+
 
 if [[ -n "${APS_INGRESS_AWS_CERT_ARN}" ]]
 then
   cat << EOF >> values.yaml
-service:
-  targetPorts:
-    http: http
-    https: http
   annotations:
-    external-dns.alpha.kubernetes.io/hostname: ${APS_HOST}.
+    # Enable PROXY protocol
+#    service.beta.kubernetes.io/aws-load-balancer-proxy-protocol: "*"
+    # Specify SSL certificate to use
     service.beta.kubernetes.io/aws-load-balancer-ssl-cert: "${APS_INGRESS_AWS_CERT_ARN}"
+    # Use SSL on the HTTPS port
+    service.beta.kubernetes.io/aws-load-balancer-ssl-ports: "https" # needed to have the correct inbound rule
+#    external-dns.alpha.kubernetes.io/hostname: ${APS_HOST}.
     service.beta.kubernetes.io/aws-load-balancer-backend-protocol: "http"
-    service.beta.kubernetes.io/aws-load-balancer-ssl-ports: "https"
-    service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: '3600'
+#    service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: "3600"
 EOF
 
   echo install external-dns with: go get -u -v github.com/kubernetes-incubator/external-dns
   echo add ${APS_HOST} to DNS with: external-dns --registry txt --txt-owner-id ${APS_HOST} --provider aws --source service --source ingress --once --dry-run
 fi
 
+# manually change the rule to by SSL to TCP
+
+# to be done on CLI as enable proxy doesn't work - https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-proxy-protocol.html
+export NGINX_INGRESS_CONTROLLER_NAME=nginx-ingress-controller
+export ELB_ADDRESS=$(kubectl get services ${NGINX_INGRESS_CONTROLLER_NAME} -o jsonpath={.status.loadBalancer.ingress[0].hostname})
+export ELB_NAME="derive manually from above up to -"
+export ELB_INSTANCE_PORT=$(kubectl get services ${NGINX_INGRESS_CONTROLLER_NAME} -o jsonpath={.spec.ports[0].nodePort})
+aws elb create-load-balancer-policy --load-balancer-name ${ELB_NAME} --policy-name my-ProxyProtocol-policy --policy-type-name ProxyProtocolPolicyType --policy-attributes AttributeName=ProxyProtocol,AttributeValue=true
+aws elb set-load-balancer-policies-for-backend-server --load-balancer-name ${ELB_NAME} --instance-port $ELB_INSTANCE_PORT
+# check with: aws elb describe-load-balancers --load-balancer-name ${ELB_NAME}
+
 # check AWS from cli
 # APS_AWS_HOSTED_ZONE=$(aws route53 list-hosted-zones --query "HostedZones[?Name == '${APS_DOMAIN}.'].Id | [0]" --output text)
 # aws route53 list-resource-record-sets --hosted-zone-id ${APS_AWS_HOSTED_ZONE} --query "ResourceRecordSets[?contains(Name,'${APS_HOST/.*}')].Name"
 # aws route53 change-resource-record-sets --hosted-zone-id ${APS_AWS_HOSTED_ZONE} --change-batch change-resource-record-sets.json
 
+
+# TEST
+# websockets working (install with npm -g install wscat): wscat --connect wss://activiti-cloud-gateway.aps2pentest.envalfresco.com/ws/graphql
+# httptrace endpoint to check x-forwarded-* headers:
+# open a URL: https://activiti-cloud-gateway.aps2pentest.envalfresco.com/activiti-cloud-modeling-backend/v2/api-docs
+# read httptrace: https://activiti-cloud-gateway.aps2pentest.envalfresco.com/activiti-cloud-modeling-backend/actuator/httptrace
+
 CHART_NAME="stable/nginx-ingress"
 
-HELM_OPTS="${HELM_OPTS} -f values.yaml --name nginx-ingress"
+HELM_OPTS="${HELM_OPTS} -f values.yaml"
+
+VERSION=${VERSION:-1.1.5}
 [[ -n "${VERSION}" ]] && HELM_OPTS="${HELM_OPTS} --version ${VERSION}"
 
 if [[ -z "${RELEASE_NAME}" ]]
