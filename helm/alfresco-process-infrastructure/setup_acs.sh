b0f7dde5f25ddd8a0dedbeb19e72bbbce6be0f10
Alexandre Chapellon <alexandre.chapellon@alfresco.com>
2021-07-30 17:24:32 +0200

    tests account must have a known password

--- helm/alfresco-process-infrastructure/setup_acs.sh
+++ helm/alfresco-process-infrastructure/setup_acs.sh
@@ -7,7 +7,7 @@ add_user_to_group "service-account-storage-service" "ALFRESCO_ADMINISTRATORS"
 
 if [[ "$LOAD_TEST_DATA" == "true" ]]
 then
-	create_user "admin.adf" "admin.adf@alfresco.com" $(generate_alfpwd)
+  create_user "admin.adf" "admin.adf@alfresco.com"
   add_user_to_group "admin.adf" "ALFRESCO_MODEL_ADMINISTRATORS"
 
   create_user "superadminuser" "superadminuser@test.com" $(generate_alfpwd)
